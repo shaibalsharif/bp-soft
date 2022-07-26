ee1c630edad04197d55b0f10de7a8c5a8aec45e0
Shaibal <shaibalsharif@gmail.com>
2022-07-27 03:33:39 +0600

    2 revisions left

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,41 +1,54 @@
 import {
-  ChevronLeftIcon, ChevronRightIcon, 
-  
+  ChevronLeftIcon, ChevronRightIcon,
+
 } from '@heroicons/react/outline'
 import { useState } from 'react'
+import FooterButton from './FooterButton'
 const ProjectCards = () => {
 
 
   const [nextclicked, setNextClicked] = useState(false)
 
   const changeClicked = () => {
-    if(nextclicked){
-      document.getElementById('secondRadio').checked=false;
+    if (nextclicked) {
+      document.getElementById('secondRadio').checked = false;
     }
-    else{
-     document.getElementById('firstRadio').checked=false;
+    else {
+      document.getElementById('firstRadio').checked = false;
     }
     setNextClicked(!nextclicked);
   }
   return (
     <div className=' '>
-      <div className={nextclicked ? 'hidden' : 'firstContainer flex flex-col justify-center md:flex-row items-center' }>
-      <ChevronRightIcon onClick={changeClicked} className='h-20 w-20 border-2 border-blue-500 rounded-[40px] text-black  absolute bg-white opacity-50 right-10' />
-       
-        <div className='Image-1 m-[20px] basis-[50%]'>
+      <div className={nextclicked ? 'hidden' : 'firstContainer flex flex-col justify-center md:flex-row items-center'}>
+        <ChevronRightIcon onClick={changeClicked} className='h-20 w-20 border-2 border-blue-500 rounded-[40px] text-black  absolute bg-white opacity-50 right-10' />
+
+        <div className='Image-1 m-[20px] -mb-[10px] basis-[50%]'>
           <img
-            className='rounded-3xl'
+            className='rounded-xl'
             src='../../assets/images/pic1.jpg' />
+
+          <FooterButton
+            classitem="absolute mt-[50px]"
+            text={"MY Dribble"}
+            icon={'FireIcon'}
+            visible={false} />
+
+
+
+
+
+
         </div>
         <div className='basis-[50%]'>
           <div className='Image-2 m-[20px] '>
             <img
-              className='rounded-3xl'
+              className='rounded-xl'
               src='../../assets/images/pic2.png' />
           </div>
           <div className='Image-3 m-[20px]'>
             <img
-              className='rounded-3xl'
+              className='rounded-xl'
               src='../../assets/images/pic3.png' />
           </div>
         </div>
@@ -44,30 +57,36 @@ const ProjectCards = () => {
       <div className={nextclicked ? 'firstContainer flex flex-col justify-center md:flex-row items-center' : 'hidden'}>
         <ChevronLeftIcon onClick={changeClicked} className='h-20 w-20 border-2 border-blue-500 rounded-[40px] text-black  bg-white opacity-50 absolute left-10' />
 
-        <div className='Image-4  m-[20px] basis-[50%]'>
+        <div className='Image-4  m-[20px] -mb-[10px] basis-[40%]'>
           <img
-            className='rounded-3xl'
+            className='rounded-xl'
             src='../../assets/images/pic4.png' />
+            <FooterButton
+            classitem="absolute mt-[50px]"
+            text={"MY Dribble"}
+            icon={'FireIcon'}
+            visible={false} />
+
         </div>
-        <div className='basis-[50%]'>
+        <div className='basis-[50%] '>
           <div className='Image-5 m-[20px]'>
             <img
-              className='rounded-3xl'
+              className='rounded-xl'
               src='../../assets/images/pic5.jpg' />
           </div>
           <div className='Image-6 m-[20px]'>
             <img
-              className='rounded-3xl'
+              className='rounded-xl'
               src='../../assets/images/pic6.jpg' />
           </div>
         </div>
 
       </div>
-     
-    
-        <input type='radio' id="firstRadio" defaultChecked  onChange={changeClicked} />
-        <input className='ml-2' type='radio' id='secondRadio'   onChange={changeClicked} />
-      
+
+
+      <input type='radio' id="firstRadio" defaultChecked onChange={changeClicked} />
+      <input className='ml-2' type='radio' id='secondRadio' onChange={changeClicked} />
+
 
     </div>
 
--- src/components/RecentProjects.js
+++ src/components/RecentProjects.js
@@ -1,22 +1,25 @@
 import FooterButton from "./FooterButton"
-import  {FireIcon} from '@heroicons/react/solid'
+import { FireIcon } from '@heroicons/react/solid'
 
 import ProjectCards from "./ProjectCards"
 const RecentProjects = () => {
   return (
-    <div className="mt-8">
-        <h1 className='text-[51px] font-bold  text-center mb[-30px]'>Recent</h1>
-        <h1 className='text-[51px] font-bold  text-center mt-[-30px]'>Projects 
+    <div className="mt-8 relative">
+     <div className="md:absolute">
+     <h1 className='text-[51px] font-bold ml-4 text-start mb[-30px]'>Recent</h1>
+      <h1 className='text-[51px] font-bold ml-4 text-start mt-[-30px]'>Projects
         <span className='font-bold text-[#689af8] text-[35px] pl-4'>+</span></h1>
 
-        <div>
-           <ProjectCards/>
-           
-        </div>
-        <FooterButton
+     </div>
+      <div>
+        <ProjectCards />
+
+      </div>
+      <FooterButton
+      classitem="md:hidden"
         text={"MY Dribble"}
-        icon= {'FireIcon'}
-        visible={true}/>
+        icon={'FireIcon'}
+        visible={true} />
     </div>
   )
 }
